8cd3f4657d5025cd84944b1eac50f43ccbe4f7d6
turboBasic ⚡💀⚡ <off@boun.cr>
2020-09-21 04:12:53 +0300

    Remove failing jobs

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -1,21 +1,17 @@
 String basePath = 'example1'
 String repo = 'sheehan/gradle-example'
 
-folder(basePath) {
-    description 'This example shows basic folder/job creation.'
-}
-
-job("$basePath/gradle-example-build") {
-    scm {
-        github repo
-    }
-    triggers {
-        scm 'H/5 * * * *'
-    }
-    steps {
-        gradle 'assemble'
-    }
-}
+// job("$basePath/gradle-example-build") {
+//     scm {
+//         github repo
+//     }
+//     triggers {
+//         scm 'H/5 * * * *'
+//     }
+//     steps {
+//         gradle 'assemble'
+//     }
+// }
 
 job("$basePath/gradle-example-deploy") {
     parameters {
